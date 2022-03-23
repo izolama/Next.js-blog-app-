1bd75aeb3a0daaa7b1feefcda8dd85bda949d02a
Faizol Ama <55181621+izolama@users.noreply.github.com>
2022-03-23 21:12:56 +0700

    firebase config

--- config/fire-config.js
+++ config/fire-config.js
@@ -1,7 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  // INSERT YOUR OWN CONFIG HERE
+  apiKey: "AIzaSyCbDnQ0F4qTzadNs1-dE2vb4qo14xpPJB8",
+  authDomain: "blog-39b7d.firebaseapp.com",
+  projectId: "blog-39b7d",
+  storageBucket: "blog-39b7d.appspot.com",
+  messagingSenderId: "496529188234",
+  appId: "1:496529188234:web:b47c55dc8cd09c9ec25473",
+  measurementId: "G-3PGYXMQZ8T"
 };
 
 try {
